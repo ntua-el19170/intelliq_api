844988671ae89e0b3581543eeca23d867d7ba20a
amarkopoulos <and.markopoulos@gmail.com>
2023-01-08 16:10:05 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+// const request = require('request-promise')
+const app = express();
+const mysql = require('mysql2')
+
+const PORT = 9103;
+const baseUrl = '/intelliq_api';
+
+app.use(express.json());
+
+app.listen(PORT,() => console.log(`server running on PORT: ${PORT}`))
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: '4321',
+    password: '12345',
+    database: 'que_database'
+}).promise();
+
+async function test() {
+    const result = await connection.query("SHOW COLUMNS FROM Question");
+    return result;
+}
+
+app.get(baseUrl,(req,res)=>{
+    res.send('welcome to intelliq')
+});
+
+app.get(`${baseUrl}/admin/healthcheck`, async (req, res) => {
+    let connectionString;
+    try {
+        await connection.connect()
+        connectionString = `server=${connection.config.host};user id=${connection.config.user};password=${connection.config.password};database=${connection.config.database}`;
+        res.send({"status":"OK", "dbconnection": connectionString});
+    }
+    catch(error) {
+        connectionString = `server=${connection.config.host};user id=${connection.config.user};password=${connection.config.password};database=${connection.config.database}`;
+        res.send({"status":"failed", "dbconnection": connectionString})
+    }
+})
+
+
+const hellovar = "hello";
+const hello_world = `${hellovar} world`
+console.log(hello_world)
+
+
+app.get(`${baseUrl}/questionnaire/:questionnaireID`, async (req, res) => {
+    const questionnaireId = req.params.questionnaireID;
+    try {
+        await connection.connect();
+        let questions = await connection.query(`SELECT * FROM Question WHERE questionnaire_id = '${questionnaireId}'`);
+        questions = questions[0];
+        let questionList = [];
+        console.log(questions.length)
+        for(const question of questions) {
+            questionList.push(
+                {
+                    "qID": question.question_id,
+                    "qtext": question.question_text,
+                    "required": question.required,
+                    "type": question.q_type
+                }
+            )
+        }
+        const questionnaire = await connection.query(`SELECT * FROM Questionnaire WHERE questionnaire_id = '${questionnaireId}'`);
+        let questionnaireTitle = questionnaire[0][0].title;
+        let keywords = questionnaire[0][0].keywords.split(',');
+        res.send({"questionnaireID": questionnaireId, "questionnaireTitle":questionnaireTitle,"keywords":keywords, "questions": questionList});
+    }
+    catch(error) {
+        res.status(500).send({ error: error });
+    }
+})
